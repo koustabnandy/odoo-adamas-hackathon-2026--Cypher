18703cde0461f6ded668817aee88023540c0453f
DDave <team@icehrm.com>
2026-03-26 14:48:47 +1100

    Update marketplace extension

--- extensions/marketplace/admin/src-ob/yakpro-po/obfuscated/Controller.php
+++ extensions/marketplace/admin/src-ob/yakpro-po/obfuscated/Controller.php
@@ -1,11 +1,11 @@
 <?php
 /*   __________________________________________________
     |  Obfuscated by YAK Pro - Php Obfuscator  2.0.14  |
-    |              on 2026-02-12 00:51:51              |
+    |              on 2026-03-19 01:36:02              |
     |    GitHub: https://github.com/pk-fr/yakpro-po    |
     |__________________________________________________|
 */
 /*
 
 */
- namespace MarketplaceAdmin; use Classes\IceController; use Classes\IceResponse; class Controller extends IceController { public function testAction($req) : IceResponse { return new IceResponse(IceResponse::SUCCESS, "\105\x63\x68\x6f\40\x66\162\157\155\40\163\145\162\166\x65\x72\72\40" . $req->data); } }
+ namespace MarketplaceAdmin; use Classes\IceController; use Classes\IceResponse; class Controller extends IceController { public function testAction($req) : IceResponse { return new IceResponse(IceResponse::SUCCESS, "\x45\143\x68\x6f\40\x66\162\x6f\x6d\x20\163\145\x72\x76\145\x72\x3a\40" . $req->data); } }
